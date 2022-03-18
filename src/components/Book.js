807db9f32f97ef41bc061fe01b91ed4eb09674e8
Akash <akashpatel18041999@gmail.com>
2022-03-18 19:01:45 +0530

    .

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,9 +4,10 @@ import { Container } from "@mui/material";
 const Book = ({ item }) => {
   console.warn(item.url, " in book");
   return (
-    <Container>
-      <p>{item.id}</p>
-      <img src={item.url} style={{ height: "100px", width: "100px" }}></img>
+    <Container style={{ border: "1px solid black" }}>
+      <p>{item.name}</p>
+      <p>Author : {item.authors}</p>
+      {/* <img src={item.url} style={{ height: "100px", width: "100px" }}></img> */}
     </Container>
   );
 };
